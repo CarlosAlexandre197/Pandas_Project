3616bce39642652a974e30860e52286c51bb9f51
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-06-24 00:15:02 -0300

    Update Pandas_Python.py
    Atualização.

--- Pandas_Python.py
+++ Pandas_Python.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests
 
-df = pd.read_excel('caminho/para/o/arquivo.xlsx')
+#df = pd.read_excel('caminho/para/o/arquivo.xlsx')
 
 url = 'https://sgi.e-boticario.com.br/Paginas/Inicializacao/AguardarAcao.aspx'
 response = requests.post(url, data=df.to_json())
